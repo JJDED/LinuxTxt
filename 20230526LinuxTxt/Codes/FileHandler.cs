963bb163422f81d4331e95d8be123574b4f16946
JJDED <jamiedelasencerie@gmail.com>
2023-06-01 11:29:08 +0200

    Add project files.

---
+++ 20230526LinuxTxt/Codes/FileHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230526LinuxTxt.Codes;
+
+internal class FileHandler
+{
+    //Update på Linux
+    //public string FileName { get; set; }
+    //public string MyUserDir { get => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), FileName }
+    //public FileHandler(string fileName) => FileName = fileName;
+    public bool CreateFile() // => File.Create(MyUserDir)
+    {
+        bool success = true;
+
+        try
+        {
+            // C:\\Users\Users\Jamie
+            string myUserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            File.Create(Path.Combine(myUserFolder, "test.txt"));
+            //File.Create();
+            Console.WriteLine("File created");
+        }
+        catch (Exception)
+        {
+            success = false;
+            Console.WriteLine("File could not create");
+        }
+
+        return success;
+    }
+}
---
+++ 20230526LinuxTxt/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _20230526LinuxTxt.Codes;
+
+Console.WriteLine("Hello, World!");
+
+FileHandler fh = new();
+
+try
+{
+    fh.CreateFile();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+int x = 1;
+
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine(x);
+    System.Threading.Thread.Sleep(1000);
+    x++;
+}
